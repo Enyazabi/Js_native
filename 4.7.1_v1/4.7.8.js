2e2a0c440c5cc833519cbe642919ef515b05c659
Enyazabi <essidanaya@gmail.com>
2019-05-08 18:47:32 +0300

    4.7.8 was completed (filterRange)

---
+++ 4.7.1_v1/4.7.8.js
@@ -0,0 +1,16 @@
+function filterRange(numbers, min,max) {
+    var result = [];
+
+    for (i = 0; i < numbers.length; i++) {
+        if (numbers[i] >= min && numbers[i] <= max) {
+            result.push(numbers[i]);
+        }
+
+    }
+
+    return result
+}
+
+var numbers = [6,9,8,1,2,7,3,4,5];
+ var filtered = filterRange(numbers, 3, 8);
+console.log (filtered);
