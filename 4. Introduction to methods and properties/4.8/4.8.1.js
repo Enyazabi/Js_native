cdb081dbf60822e10b3927d89cca92c4ad435b7d
Enyazabi <essidanaya@gmail.com>
2019-05-15 19:53:09 +0300

    4.8.2 compl

--- 4. Introduction to methods and properties/4.8/4.8.1.js
+++ 4. Introduction to methods and properties/4.8/4.8.1.js
@@ -1,14 +1,17 @@
 function addClass (obj, cls) {
    var words = obj.className;
-
    for (var i = 0; i < words.length; i++) {
-       if (words[i] === cls) return;
-   }
 
-        words.push(cls);
-        obj.className = words.join('');
+   }
+    if (words.indexOf(cls)) {
+        if (words.indexOf()) {
+            words.push(cls);
+            obj.className = words.join('');
+        }
+
     }
 
+        }
 
 var obj = {
     className: 'open menu'
