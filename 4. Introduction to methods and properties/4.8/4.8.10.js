5bef328c5e06304edb13914938ef6ef099a5bbfe
Enyazabi <essidanaya@gmail.com>
2019-05-29 21:57:05 +0300

    4.8.10 in progress

--- 4. Introduction to methods and properties/4.8/4.8.10.js
+++ 4. Introduction to methods and properties/4.8/4.8.10.js
@@ -1,7 +1,23 @@
 var arr = ["воз", "киборг", "корсет", "ЗОВ", "гробик", "костер", "сектор"];
 
 function aclean(arr) {
-    
+    var arrClean = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        temp = arr[i].split("").sort().join("");
+        flag = true;
+
+        for (var j = 0; j < arrClean.length; j++) {
+
+            if ( arrClean[i] != arrClean[j]) {
+                flag = false;
+            }
+        }
+        if ( flag === true) {
+            arrClean.push(arr[i]);
+        }
+
+    }
 }
 
 console.log( aclean(arr));
