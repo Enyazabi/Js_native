cc6d7f8a5f246577d57f73aa85655cfde5df06ca
Enyazabi <essidanaya@gmail.com>
2019-05-29 22:33:07 +0300

    4.8.10 was completed

--- 4. Introduction to methods and properties/4.8/4.8.10.js
+++ 4. Introduction to methods and properties/4.8/4.8.10.js
@@ -4,20 +4,11 @@ function aclean(arr) {
     var arrClean = [];
 
     for (var i = 0; i < arr.length; i++) {
-        temp = arr[i].split("").sort().join("");
-        flag = true;
-
-        for (var j = 0; j < arrClean.length; j++) {
-
-            if ( arrClean[i] != arrClean[j]) {
-                flag = false;
-            }
-        }
-        if ( flag === true) {
-            arrClean.push(arr[i]);
-        }
-
+        f = arr[i].split("").sort().join("").toLowerCase();
+        arrClean.push(f);
     }
+
+    return [...new Set(arrClean)];
 }
 
 console.log( aclean(arr));
