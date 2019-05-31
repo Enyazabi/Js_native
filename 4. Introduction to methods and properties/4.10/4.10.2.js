aff3cc27a6ca88df2c5e451644def14d981b4e24
enya <essidanaya@gmail.com>
2019-05-31 16:15:07 +0300

    4.10.2 was completed

---
+++ 4. Introduction to methods and properties/4.10/4.10.2.js
@@ -0,0 +1,20 @@
+function sum() {
+    let arr = [];
+    for(let i = 0; i < arguments.length; i++) {
+        arr[i] = arguments[i];
+    }
+
+    let sum = 0;
+    for(let j = 0; j < arr.length; j++) {
+         let fsum = sum + arr[j];
+        sum = fsum;
+    }
+
+    console.log(sum);
+}
+
+sum();
+sum(1);
+sum(1, 2);
+sum(1, 2, 3);
+sum(1, 2, 3, 4);
