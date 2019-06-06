e76af09a0766922ee9feff361d78eff3b441dcd4
enya <essidanaya@gmail.com>
2019-06-06 21:44:45 +0300

    4.9 fixed

--- 4. Introduction to methods and properties/4.9/4.9.1.js
+++ 4. Introduction to methods and properties/4.9/4.9.1.js
@@ -1,7 +1,4 @@
 let arr = ["Is", "there", "life", "on", "Mars"];
-
-let arrLengths = arr.map(function (word) {
-    return word.length;
-});
+let arrLengths = arr.map((word)=>word.length);
 
 console.log(arrLengths);
--- 4. Introduction to methods and properties/4.9/4.9.2v3.js
+++ 4. Introduction to methods and properties/4.9/4.9.2v3.js
@@ -1,5 +1,4 @@
 function sum() {
-
     return [...arguments].reduce( (a, b) => a + b, 0 );
 }
 
