8862f3b7dcb685c41eee3aac700c969fc55d5d50
enya <essidanaya@gmail.com>
2019-06-07 19:49:36 +0300

    4.11.2 was completed

--- 4. Introduction to methods and properties/4.11/4.11.2.js
+++ 4. Introduction to methods and properties/4.11/4.11.2.js
@@ -1,29 +1,8 @@
-let date = new Date(2012, 0, 3);
+let date = new Date(2012,0,3);
+let days = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
 
-function getWeekDay (date) {
-    let dayOfWeek = date.getDay();
-
-    let obj = {
-        0: "вс",
-        1: "пн",
-        2: "вт",
-        3: "ср",
-        4: "чт",
-        5: "пт",
-        6: "сб"
-    }
-
-    let array =[];
-
-        for (let key in obj) {
-            array[key] = obj[key];
-        }
-
-        key = 0;
-            while (key != dayOfWeek) {
-                key++;
-            }
-        console.log(obj[key]);
+function getWeekDay(date) {
+    return days[date.getDay()];
 }
 
-getWeekDay(date);
+console.log( getWeekDay(date) );
