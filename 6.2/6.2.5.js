b6f2d5f0fb611e0e326d35424c2ebb5ab91b679f
enya <essidanaya@gmail.com>
2019-06-25 17:44:12 +0300

    commented code was removed

--- 6.2/6.2.5.js
+++ 6.2/6.2.5.js
@@ -1,23 +1,3 @@
-/*function sum(a){
-    function curSum (b) {
-
-        return sum(a+b);
-    }
-
-    curSum.toString = function() {
-
-        return a;
-    };
-
-    return curSum;
-}
-
-console.log(+sum(1)(2));
-console.log(+sum(1)(2)(3));
-console.log(+sum(5)(-1)(2));
-console.log(+sum(6)(-1)(-2)(-3));
-console.log(+sum(0)(1)(2)(3)(4)(5));*/
-
 function sum (a) {
 
     const curSum = (b) => sum(a + b);
